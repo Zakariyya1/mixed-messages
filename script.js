38bafc665fb7e69ba072b93a7985b428880d6474
Zakariyya1 <zakp100@gmail.com>
2024-09-06 15:24:17 +0100

    add functionality to randomly generate the 3 best adjectives describing the user

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ const adjectiveList3 = [
   'uptight'
 ];
 
-console.log(adjectiveList1[3], adjectiveList2[2], adjectiveList3[1]);
+const insultGenerator = (adjArr1, adjArr2, adjArr3) => {
+  const adjList1 = adjArr1.slice();
+  const adjList2 = adjArr2.slice();
+  const adjList3 = adjArr3.slice();
+
+  const randomNumber = () => Math.floor(Math.random() * 5);
+
+  return `The best 3 words to describe you are: ${adjList1[randomNumber()]}, ${
+    adjList2[randomNumber()]
+  } and ${adjList3[randomNumber()]}.`;
+};
+
+console.log(insultGenerator(adjectiveList1, adjectiveList2, adjectiveList3));
