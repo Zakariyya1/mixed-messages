d062d984f65a08804122c2a053d3ed7663b5d4b0
Zakariyya Patel <zp_home@Zakariyyas-MacBook-Pro.local>
2024-09-06 14:55:10 +0100

    add adjecive lists for the generator to choose from

---
+++ script.js
@@ -0,0 +1,19 @@
+const adjectiveList1 = [
+  'arrogant',
+  'annoying',
+  'combative',
+  'disturbed',
+  'drab'
+];
+
+const adjectiveList2 = ['lazy', 'mushy', 'obnoxious', 'prickly', 'repulsive'];
+
+const adjectiveList3 = [
+  'thoughtless',
+  'unsightly',
+  'wrong',
+  'troubled',
+  'uptight'
+];
+
+console.log(adjectiveList1[3], adjectiveList2[2], adjectiveList3[1]);
