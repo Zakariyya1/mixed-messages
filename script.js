6d77626145bbb05643b2e13487ccb7089eb915b4
Zakariyya1 <zakp100@gmail.com>
2024-10-09 12:02:13 +0100

    refactor

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ const adjectiveList3 = [
   'uptight'
 ];
 
+const randomNumber = () => Math.floor(Math.random() * 5);
+
 const insultGenerator = (adjArr1, adjArr2, adjArr3) => {
   const adjList1 = adjArr1.slice();
   const adjList2 = adjArr2.slice();
   const adjList3 = adjArr3.slice();
-
-  const randomNumber = () => Math.floor(Math.random() * 5);
 
   return `The best 3 words to describe you are: ${adjList1[randomNumber()]}, ${
     adjList2[randomNumber()]
